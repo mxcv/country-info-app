bb0b4b7ce905621c2fa766ba57b0e92244a9ebd8
Maksym Chervinskyi <mxcs02@gmail.com>
2025-03-01 14:15:09 +0200

    feat: configure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        url: configService.get('DATABASE_URL'),
+        synchronize: configService.get('DATABASE_SYNCHRONIZE') === 'true',
+        entities: [path.join(__dirname, '**/*.entity.js')],
+      }),
+    }),
+  ],
 })
 export class AppModule {}
